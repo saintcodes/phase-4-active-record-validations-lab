c9f4c6af1000727855bcff79bceee40e3f77213b
Michelle <clairsin@gmail.com>
2022-06-30 14:08:01 -0400

    commit tree bec0490383e8757ef0ca31669c5117f295205562

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+  validate is_clickbait?
+
+  def is_clickbait
+    
+  end
+
 end
